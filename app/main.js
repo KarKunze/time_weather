4c0bf33ab1cd8b3a4981981947b2c17d9efc472d
Karla Kunze <karlalkunze@gmail.com>
2018-06-14 21:32:26 -0400

    added date

--- app/main.js
+++ app/main.js
@@ -1,3 +1,8 @@
+let currentDate = new Date();
+let dateStr = currentDate.toLocaleString();
+document.getElementById("date").innerHTML = dateStr;
+
+
 let date = document.getElementById("date");
 let city = document.getElementById("city");
 let tempF = document.getElementById("tempF");
@@ -8,6 +13,7 @@ let humidity = document.getElementById("humidity");
 let wind = document.getElementById("wind");
 let condition = document.getElementById("condition");
 let weatherImage = document.getElementById('weatherImage');
+let bg = document.getElementById('bg');
 
 let apiRequest;
 let appId = "d710a4aa5b152e9b5fe7739a6ccca894";
@@ -44,14 +50,15 @@ function catchResponse() {
     city.innerHTML = response.name;
     tempF.innerHTML = convertKtoF(response.main.temp) + "&degF";
     // tempC.innerHTML = convertKtoC(response.main.temp) + "&degC";
-    highTemp.innerHTML = convertKtoF(response.main.temp_max);
-    lowTemp.innerHTML = convertKtoF(response.main.temp_min);
-    humidity.innerHTML = response.main.humidity;
-    wind.innerHTML = response.wind.speed;
+    highTemp.innerHTML = "High " + convertKtoF(response.main.temp_max) + "&degF";
+    lowTemp.innerHTML = "Low " + convertKtoF(response.main.temp_min) + "&degF";
+    humidity.innerHTML = "Humidity " + response.main.humidity + "&#37;";
+    wind.innerHTML = "Wind speed " + convertMperStoMPH(response.wind.speed) + " mph";
     condition.innerHTML = response.weather[0].main;
-    console.log(response.weather);
-    // displayImage(response.weather.main);
+    // console.log(response.weather);
+    displayImage.innerHTML = displayImage(response.weather[0].main);
 		// output.style.display = 'block';
+    bg.style.backgroundImage = displayImage(response.weather[0].main);
   }
 }
 
@@ -64,4 +71,34 @@ function catchResponse() {
   function convertKtoC(kelvin) {
     let celsius = kelvin - 273.15;
     return Math.round(celsius);
+  }
+
+  function convertMperStoMPH(MperS) {
+    let mph = MperS * 2.2369;
+    return Math.round(mph*10)/10;
+  }
+
+  function displayImage(condition) {
+    if (condition.toLowerCase().includes("clouds")) {
+      return 'url("images/blue-clouds-day-53594.jpg")';
+    }
+    else if (condition.toLowerCase().includes("rain")) {
+      return 'url("images/abstract-background-blue-311039.jpg")';
+    }
+    else if (condition.toLowerCase().includes("clear")) {
+      return 'url("images/air-atmosphere-blue-96622.jpg")';
+    }
+    else if (condition.toLowerCase().includes("thunderstorm")) {
+      return 'url("images/cloudiness-clouds-cloudscape-1074428.jpg")';
+    }
+    else if (condition.toLowerCase().includes("mist")) {
+      return 'url("images/blur-calm-waters-dawn-395198 (1).jpg")';
+    }
+    else if (condition.toLowerCase().includes("snow")) {
+      return 'url("images/abstract-blur-bright-286198 (1).jpg")';
+    }
+    else {
+      return 'url("images/galaxy-infinity-milky-way-110854.jpg")';
+    }
+
   }
